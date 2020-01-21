981815c0df7b8cb05a7bdcb0b841cecdca7760c0
91let@naver.com <91let@naver.com>
2020-01-21 18:08:07 +0900

    front-proto

--- routing-practice/src/components/Home.tsx
+++ routing-practice/src/components/Home.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import './Home.css';
+import SearchBar from './Home/SearchBar';
 
-
-export const Home = () => <h1>Home Page</h1>;
+export const Home = () => (
+    <>
+        <h1 className="Title">Alcohol Holic</h1>
+        <SearchBar />
+    </>
+);
 
 export default Home;
---
+++ routing-practice/src/components/Home/SearchBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SearchBar.css';
+
+export const SearchBar = () => (
+    <input className="SearchBar" placeholder="상품명, 주종 또는 키워드를 입력해 주세요"/>
+);
+
+export default SearchBar;
