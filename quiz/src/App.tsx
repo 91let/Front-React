01862d1fadeb51302f7ebe85ce5c26d76254ec46
91let <91let@naver.com>
2020-03-10 16:49:59 +0900

    quiz

---
+++ quiz/src/App.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const restUrl = "/quiz/";
+  const [question, setQuestion] = useState("답변에 SSAFY를 입력해 주세요");
+  const [url, setUrl] = useState("alpha");
+  const [nickname, setNickname] = useState("");
+  const [yourAnswer, setYourAnswer] = useState("");
+  const [result, setResult] = useState(true);
+
+  const config = {
+    headers: { 
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+     },
+  }
+
+  const send = () => {
+    axios
+      .post(restUrl + url, {
+          "nickname" : nickname,
+          "yourAnswer" : yourAnswer
+        },
+        config
+      )
+      .then(response => {
+        console.log(nickname)
+        console.log(yourAnswer)
+        if(response.data.code === 200) {
+          setResult(true);
+          setUrl(response.data.nextUrl)
+          setQuestion(response.data.question)
+          console.log("success");
+          console.log(response.data);
+        }
+        else{
+          setResult(false);
+          console.log(response.data);
+        }
+      })
+  }
+
+  const nicknameChange = (e: { target: { value: React.SetStateAction<string> } }) => {
+    setNickname(e.target.value.toString());
+  }
+
+  const ansChange = (e: { target: { value: React.SetStateAction<string> } }) => {
+    setYourAnswer(e.target.value.toString());
+  }
+
+  return (
+    <div>
+      <label>{url}</label>
+      <br/>
+      <label>{question}</label>
+      <br/>
+      <label>지역반이름</label>
+      <input type="text" value={nickname} onChange={nicknameChange}/>
+      <br/>
+      <label>정답</label>
+      <input type="text" value={yourAnswer} onChange={ansChange}/>
+      {result? <label></label>: <label>오답</label>}
+      <br/>
+      <button onClick={send}>제출</button>
+    </div>
+  );
+}
+
+export default App;
